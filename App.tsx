f1f56cc6ab29d9c4c99aecee1e738d66d629bf8e
Michael Hughes <microneesia79@gmail.com>
2026-06-23 17:01:30 -0700

    Scan reviewed starter file contents in safety report

--- App.tsx
+++ App.tsx
@@ -130,7 +130,7 @@ export default function App() {
 
   const allStarterFilesApproved = reviewedStarterFiles.length > 0 && approvedStarterFileCount === reviewedStarterFiles.length;
   const allStarterIssuesApproved = reviewedStarterIssues.length === approvedStarterIssueCount;
-  const safetyReport = useMemo(() => scanRepoPlan(repoPlan), [repoPlan]);
+  const safetyReport = useMemo(() => scanRepoPlan(repoPlan, reviewedStarterFiles), [repoPlan, reviewedStarterFiles]);
   const receipts = useMemo(() => createSeedReceipts(repoPlan, safetyReport), [repoPlan, safetyReport]);
   const dryRunWriterResult = useMemo(() => dryRunWriterAdapter.dryRun({
     approvedByUser: allStarterFilesApproved && allStarterIssuesApproved,
